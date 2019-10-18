c323bcc6d6190c293f457d32d7a87279187c9882
jdelak <delakdev@gmail.com>
2019-10-18 10:08:09 +0200

    ajout de bootstrap + modif vue create player

--- riskinspace-react-front/src/component/CreatePlayer.jsx
+++ riskinspace-react-front/src/component/CreatePlayer.jsx
@@ -2,47 +2,53 @@ import React, { Component } from 'react';
 import RiskinspaceService from '../service/RiskinspaceService';
 class CreatePlayer extends Component {
 
-    constructor(props) {
-        super(props)
-        this.refreshCreatePlayer = this.refreshCreatePlayer.bind(this)
-        this.state = {
-            species: []
-        }
+  constructor(props) {
+    super(props)
+    this.refreshCreatePlayer = this.refreshCreatePlayer.bind(this)
+    this.state = {
+      species: []
     }
+  }
 
-    componentDidMount() {
-        this.refreshCreatePlayer();
-    }
+  componentDidMount() {
+    this.refreshCreatePlayer();
+  }
 
-    refreshCreatePlayer() {
-        RiskinspaceService.home()//HARDCODED
-            .then(
-                response => {
-                    console.log(response);
-                    this.setState({ species: response.data })
-                }
+  refreshCreatePlayer() {
+    RiskinspaceService.home()//HARDCODED
+    .then(
+      response => {
+        console.log(response);
+        this.setState({ species: response.data })
+      }
+    )
+  }
+
+  render(){
+    return (
+      <div className="container-fluid">
+      <div className="d-flex justify-content-center">
+      <div className="">
+      <form>
+        <div className="form-group">
+          <label htmlFor="name">Nom Joueur 1 : </label>
+          <input type="text" name="player-name" id="name" className="form-control"/>
+        </div>
+        <div className="form-group">
+          <select name="player-species">
+          <option value="">Sélectionner une race</option>
+          {
+            this.state.species.map(
+              specie => <option value={specie.speciesId}>{specie.speciesName}</option>
             )
-    }
-
-    render(){
-        return (
-            <div>
-            {this.state.texte}
-            <form>
-            <label htmlFor="name">Nom Joueur 1 : </label>
-            <input type="text" name="player-name" id="name" />
-            <select name="player-species">
-            <option value="">Sélectionner une race</option>
-            {
-              this.state.species.map(
-                specie => <option value={specie.speciesId}>{specie.speciesName}</option>
-              )
-            }
-
-           </select>
-
-           <label htmlFor="name">Nom Joueur 2 : </label>
-          <input type="text" name="player-name2" id="name2" />
+          }
+          </select>
+        </div>
+        <div className="form-group">
+          <label htmlFor="name">Nom Joueur 2 : </label>
+          <input type="text" name="player-name2" id="name2"  className="form-control"/>
+        </div>
+        <div className="form-group">
           <select name="player-species2">
           <option value="">Sélectionner une race</option>
           {
@@ -51,13 +57,14 @@ class CreatePlayer extends Component {
             )
           }
           </select>
-
-         <input type="submit" />
-            </form>
-            </div>
-
-        );
-    }
+        </div>
+        <input type="submit" value="jouer" className="btn btn-primary" id="jouer" />
+      </form>
+      </div>
+      </div>
+      </div>
+    );
+  }
 
 }
 
