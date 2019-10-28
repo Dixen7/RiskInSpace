24a622679a1c7e47939ce4365c810ae41b189daa
jdelak <delakdev@gmail.com>
2019-10-28 09:35:27 +0100

    ?

--- riskinspace-react-front/src/component/Planets.jsx
+++ riskinspace-react-front/src/component/Planets.jsx
@@ -2,9 +2,9 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import RiskinspaceService from '../service/RiskinspaceService';
 import {Button,Modal} from 'react-bootstrap';
-import axios from 'axios'
 
 class Planets extends Component {
+
 
     constructor() {
       super();
@@ -15,21 +15,13 @@ class Planets extends Component {
         planetsPlayer1:0,
         planetsPlayer2:0,
         currentPlayer:'',
-        shipCount:0,
       };
+
+
     }
 
     componentDidMount() {
       this.refresh();
-    }
-
-    changecolor() {
-      let root = document.documentElement;
-      if (Planet.props.currentPlayer == Planet.props.owner) {
-        root.style.setProperty('--planet-color', 'green');
-      } else {
-        root.style.setProperty('--planet-color', 'red');
-      }
     }
 
     refresh() {
@@ -37,7 +29,7 @@ class Planets extends Component {
       .then(
         response => {
           console.log(response);
-          this.setState({planets: response.data.planets})
+          this.setState({ planets: response.data.planets })
           this.setState({player1:response.data.player1})
           this.setState({player2:response.data.player2})
           this.setState({currentPlayer:this.state.player1.playerName})
@@ -45,7 +37,6 @@ class Planets extends Component {
           this.setState({planetsPlayer2:response.data.countPlanetPlayer2})
           this.setState({player1Species:response.data.player1.species.speciesName})
           this.setState({player2Species:response.data.player2.species.speciesName})
-          this.setState({shipCount:response.data.shipCount})
         }
       )
     }
@@ -65,11 +56,10 @@ class Planets extends Component {
             <p>Race : {this.state.player1Species}</p>
             <p>Nb Planètes : {this.state.planetsPlayer1}</p>
           </div>
-
           <div className="tour">
-            <h3>Tour de {this.state.currentPlayer}</h3>
+            <h3>Tour de </h3>
+            <p>{this.state.currentPlayer}</p>
           </div>
-
           <div className="joueur2">
             <h2>{this.state.player2.playerName}</h2>
             <p>Race : {this.state.player2Species}</p>
@@ -82,20 +72,23 @@ class Planets extends Component {
           </button>
             <button className="risk-button" id="button2" onClick={this.changePlayer}>Fin de tour</button>
           </div>
-
           <div className="grid-container">
             {this.state.planets.map(planet => {
               return (
                 <Planet
-                 id={planet.planetId} name={planet.planetName} key={planet.planetId} owner={planet.planetOwner.playerName} nbships={planet.planetShipsNbr} currentPlayer={this.state.currentPlayer} shipCount={this.state.shipCount}
+                 id={planet.planetId} name={planet.planetName} key={planet.planetId} owner={planet.planetOwner.playerName} nbships={planet.planetShipsNbr} currentPlayer={this.state.currentPlayer}
                 />
+
               );
             })}
           </div>
 
         </div>
+
+
       );
     }
+
   }
 
   class Planet extends Component {
@@ -108,13 +101,9 @@ class Planets extends Component {
         this.state = {
           show:false,
           currentPlayer:props.currentPlayer,
-          planetOwner:props.owner,
-          shipCount:props.shipCount,
-          planetId:props.id
+          planetOwner:props.owner
         }
     }
-
- 
 
     handleClose(e){
        e.stopPropagation();
@@ -135,47 +124,39 @@ class Planets extends Component {
       </form>;
     }
 
-    placeShip = (e) =>{
+    placeShip(e){
       console.log('placeShip');
-      this.setState({ [e.target.name]: e.target.value });
-      let planet = {
-        "planetId": parseInt(this.state.planetId)
-      };
-      axios.post('http://localhost:8080/gamephase', planet)
-		  .then(function (response) {
-        this.setState({shipCount: this.state.shipCount - 1})
-		  })
-		  .catch(function (error) {
-		    console.log(error);
-		  });
     }
 
     render() {
+
       let text;
       let action;
-
       if(this.props.currentPlayer != this.props.owner){
-        text = "Attaquer";
+        text = "Attack";
         action = this.attack;
       } else {
-        text = "Placer troupes";
+        text = "placer Troupe";
         action = this.placeShip;
       }
+
+
 
       return (
         <article className={'card card--'+this.props.id} onClick={this.handleShow}>
         <div className="card__planet">
           <div className="planet__atmosphere">
             <div className="planet__surface"></div>
+
+
           </div>
-          <input type="hidden" id={"planetId-"+this.props.id} name="planetId" value={this.props.id} />
         </div>
         {/* <div className="card__info">
           <h2 className="info__title">{this.props.name}</h2>
           </div> */}
           <Modal show={this.state.show} animation={false}>
               <Modal.Title>{this.props.name}</Modal.Title>
-            <Modal.Body><br/>Proprietaire : {this.props.owner} <br/>Nb de vaisseaux : {this.props.nbships}<br/>Nd de vaisseaux restants a placer : {this.props.shipCount}</Modal.Body>
+            <Modal.Body><br/>Proprietaire : {this.props.owner} <br/>Nb de vaisseaux : {this.props.nbships}</Modal.Body>
             <Modal.Footer>
               <Button variant="primary" onClick={action}>
                 {text}
@@ -186,6 +167,7 @@ class Planets extends Component {
             </Modal.Footer>
           </Modal>
        </article>
+
       );
     }
   }
